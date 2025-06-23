e6f506405d5f85a4b0d2b6b7328f626c84fd75b4
pranavsharmaa1908 <pranavsharmaa1908@gmail.com>
2025-06-23 09:25:22 +0530

    gradle initialisation

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [ python312 python312Packages.pip python312Packages.gcsa ];
+    nativeBuildInputs = with pkgs.buildPackages; [ python312 python312Packages.pip python312Packages.gcsa kotlin jdk17 gradle ];
 }
